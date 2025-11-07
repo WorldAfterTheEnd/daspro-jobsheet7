da329c6d58d666c9710a7dd5459d8a8ebf8a9ced
WorldAfterTheEnd <mabhiramaputra@gmail.com>
2025-11-07 08:41:35 +0700

    Jumlah mahasiswa lulus dan tidak

--- siakadFor16.java
+++ siakadFor16.java
@@ -4,20 +4,30 @@ public class siakadFor16 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         double nilai, tertinggi=0, terendah=100;
+        int jumlahTidakLulus=0, jumlahLulus=0;
+
+        //If nilai < 60 = tidak lulus
 
         for (int i = 1; i <= 10; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
             nilai = scanner.nextDouble();
-
+            
             if (nilai > tertinggi) {
                 tertinggi = nilai;
             }
             if (nilai < terendah) {
                 terendah = nilai;
             }
+
+            if (nilai < 60) {
+                jumlahTidakLulus++;
+            } else {
+                jumlahLulus++;
+            }
         }
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
-        
+        System.out.println("Jumlah mahasiswa tidak lulus: " + jumlahTidakLulus);
+        System.out.println("Jumlah mahasiswa lulus: " + jumlahLulus);
     }
 }
