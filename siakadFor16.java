f1bdac8ac0e0d7da4ba38965f1ea1b3e430f8095
WorldAfterTheEnd <mabhiramaputra@gmail.com>
2025-11-07 08:56:10 +0700

    Grading dan Batsan nilai valid

--- siakadFor16.java
+++ siakadFor16.java
@@ -11,7 +11,35 @@ public class siakadFor16 {
         for (int i = 1; i <= 10; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
             nilai = scanner.nextDouble();
-            
+
+            if (nilai < 0 || nilai > 100) {
+                System.out.println("Nilai tidak valid. Masukkan nilai antara 0 hingga 100.");
+                i--; // Mengulangi iterasi untuk mahasiswa yang sama
+                continue;
+            }
+            // nilai yang valid dikelompokkan ke dalam kategori huruf A (80 < nilai ≤ 100), B+ (73 < nilai ≤ 80), B (65 < nilai ≤ 73), C+ (60 < nilai ≤ 65), C (50 < nilai ≤ 60), D (39 < nilai ≤ 50), dan E (nilai ≤ 39).
+            if (nilai > 80 && nilai <= 100){
+                System.out.println("Nilai mahasiswa ke-" + i + " adalah A ");
+            }
+            else if (nilai > 73 && nilai <= 80){
+                System.out.println("Nilai mahasiswa ke-" + i + " adalah B+ ");
+            }
+            else if (nilai > 65 && nilai <= 73){
+                System.out.println("Nilai mahasiswa ke-" + i + " adalah B ");
+            }
+            else if (nilai > 60 && nilai <= 65){
+                System.out.println("Nilai mahasiswa ke-" + i + " adalah C+ ");
+            }
+            else if (nilai > 50 && nilai <= 60){
+                System.out.println("Nilai mahasiswa ke-" + i + " adalah C ");
+            }
+            else if (nilai > 39 && nilai <= 50){
+                System.out.println("Nilai mahasiswa ke-" + i + " adalah D ");
+            }
+            else {
+                System.out.println("Nilai mahasiswa ke-" + i + " adalah E ");
+            }
+
             if (nilai > tertinggi) {
                 tertinggi = nilai;
             }
